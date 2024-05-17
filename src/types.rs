f08de39f5daa8cecc39af599758be1529a18f3a3
dimazhornyk <dima.zhornyk@gmail.com>
2024-05-17 14:00:10 +0200

    make types public

--- src/types.rs
+++ src/types.rs
@@ -2,10 +2,10 @@ pub type DataAvailabilityError = String;
 
 #[derive(Default)]
 pub struct DispatchResponse {
-    pub(crate) blob_id: Vec<u8>,
+    pub blob_id: Vec<u8>,
 }
 
 #[derive(Default)]
 pub struct InclusionData {
-    data: Vec<u8>,
+    pub data: Vec<u8>,
 }
