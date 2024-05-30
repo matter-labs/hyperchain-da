131c833531da2b681053ead3c580fc33ec9e967c
dimazhornyk <dima.zhornyk@gmail.com>
2024-05-30 21:51:39 +0200

    add display

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,16 @@
+use std::fmt::Display;
 use serde::Serialize;
 
+#[derive(Debug)]
 pub struct DAError {
     error: anyhow::Error,
     is_transient: bool,
+}
+
+impl Display for DAError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "DAError: {}, is_transient: {}", self.error, self.is_transient)
+    }
 }
 
 /// DispatchResponse is the response received from the DA layer after dispatching a blob.
