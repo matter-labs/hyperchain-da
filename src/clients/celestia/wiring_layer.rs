5ff4a763a35f0b711a94fc0a2f45b4e956b13c26
dimazhornyk <dima.zhornyk@gmail.com>
2024-06-04 16:59:55 +0200

    remove unneeded files

--- src/clients/celestia/wiring_layer.rs
+++ src/clients/celestia/wiring_layer.rs
@@ -1,26 +1,23 @@
-use zksync_config::configs::da_dispatcher::{DADispatcherConfig};
-use zksync_env_config::FromEnv;
+use crate::clients::celestia::{config::CelestiaConfig, client::CelestiaClient};
 
 use zksync_node_framework::{
     service::ServiceContext,
     wiring_layer::{WiringError, WiringLayer},
 };
-
-use crate::clients::celestia::{CelestiaClient, config::CelestiaConfig};
-use crate::clients::celestia::resource::CelestiaClientResource;
-use crate::DataAvailabilityClient;
+use zksync_node_framework::implementations::resources::da_client::DAClientResource;
+use zksync_da_client::DataAvailabilityClient;
 
 #[derive(Debug)]
 pub struct CelestiaWiringLayer {
-    da_config: DADispatcherConfig,
+    config: CelestiaConfig,
 }
 
 impl CelestiaWiringLayer {
     pub fn new(
-        da_config: DADispatcherConfig,
+        config: CelestiaConfig,
     ) -> Self {
         Self {
-            da_config,
+            config,
         }
     }
 }
@@ -32,10 +29,9 @@ impl WiringLayer for CelestiaWiringLayer {
     }
 
     async fn wire(self: Box<Self>, mut context: ServiceContext<'_>) -> Result<(), WiringError> {
-        let config = CelestiaConfig::from_env();
-        let client: Box<dyn DataAvailabilityClient> = Box::new(CelestiaClient::new(config?));
+        let client: Box<dyn DataAvailabilityClient> = Box::new(CelestiaClient::new()?);
 
-        context.insert_resource(CelestiaClientResource(client))?;
+        context.insert_resource(DAClientResource(client))?;
 
         Ok(())
     }
--- src/config.rs
+++
@@ -1,9 +0,0 @@
-use serde::Deserialize;
-use crate::clients::celestia::config::CelestiaConfig;
-
-/// Enum representing the configuration for the different data availability layers
-#[derive(Clone, Debug, PartialEq, Deserialize)]
-#[serde(tag = "client_name")]
-pub enum DALayerConfig {
-    Celestia(CelestiaConfig),
-}
