96b495dbb3ae9257a87a95ec910507aa9e99f5f6
c-node <chuckhead7@gmail.com>
2024-06-10 19:58:52 -0400

    working on EVM types...

--- src/clients/celestia/evm_types.rs
+++ src/clients/celestia/evm_types.rs
@@ -1,5 +1,11 @@
 use alloy_sol_macro::sol;
-use nmt_rs::NamespaceProof;
+use nmt_rs::{
+    NamespaceProof,
+    NamespaceId,
+    NamespacedHash,
+};
+
+const CELESTIA_NS_ID_SIZE: usize = 29;
 
 sol! {
     struct BinaryMerkleProof {
@@ -34,5 +40,52 @@ sol! {
         bytes1 version;
         // The namespace ID.
         bytes28 id;
+    }
+}
+
+impl From<NamespaceId<CELESTIA_NS_ID_SIZE> for Namespace {
+    fn from(namespace_id: NamespaceId<CELESTIA_NS_ID_SIZE>) -> Self {
+        Self {
+            version: namespace_id.version,
+            id: namespace_id.id,
+        }
+    }
+
+}
+
+impl From<NamespacedHash<CELESTIA_NS_ID_SIZE>> for NamespaceNode {
+    fn from(hash: NamespacedHash<CELESTIA_NS_ID_SIZE>) -> Self {
+        Self {
+            min: hash.min_namespace(),
+            max: hash.max_namespace(),
+            digest: hash.hash,
+        }
+    }
+
+}
+
+impl From<NamespaceProof> for NamespaceMerkleMultiproof {
+    fn from(proof: NamespaceProof) -> Self {
+        match proof {
+            NamespaceProof::AbsenceProof { .. } => {
+                panic!("cannot convert absence proof to multiproof");
+            }
+            NamespaceProof::PresenceProof { proof, .. } => {
+                let sideNodes = proof
+                    .siblings
+                    .iter()
+                    .map(|sibling| NamespaceNode {
+                        min: sibling.min.clone(),
+                        max: sibling.max.clone(),
+                        digest: sibling.digest.clone(),
+                    })
+                    .collect();
+                Self {
+                    beginKey: proof.begin_key,
+                    endKey: proof.end_key,
+                    sideNodes,
+                }
+            }
+        }
     }
 }
