c73fa6fdbada9f6213e799d68a683693555f43ad
c-node <chuckhead7@gmail.com>
2024-07-26 22:35:38 -0400

    cleanup

--- contracts/contracts/l2-contracts/Celestia.sol
+++ contracts/contracts/l2-contracts/Celestia.sol
@@ -5,12 +5,6 @@ pragma solidity 0.8.24;
 // solhint-disable gas-custom-errors, reason-string
 
 import {IL2DAValidator} from "./interfaces/IL2DAValidator.sol";
-/*import {BinaryMerkleMultiproof} from "@blobstreamMain/lib/tree/binary/BinaryMerkleMultiproof.sol";
-import {BinaryMerkleTree} from "@blobstreamMain/lib/tree/binary/BinaryMerkletree.sol";
-import {NamespaceMerkleTree} from "@blobstreamMain/lib/tree/namespace/NamespaceMerkletree.sol";
-import {NamespaceNode} from "@blobstreamMain/lib/tree/namespace/NamespaceNode.sol";
-import {Namespace} from "@blobstreamMain/lib/tree/Types.sol";
-import {NamespaceMerkleMultiproof} from "@blobstreamMain/lib/tree/namespace/NamespaceMerkleMultiproof.sol";*/
 import "../../lib/blobstream-contracts.git/src/lib/tree/binary/BinaryMerkleMultiproof.sol";
 import "../../lib/blobstream-contracts.git/src/lib/tree/binary/BinaryMerkletree.sol";
 import "../../lib/blobstream-contracts.git/src/lib/tree/namespace/NamespaceMerkletree.sol";
@@ -45,35 +39,41 @@ contract CelestiaL2DAValidator is IL2DAValidator {
         // Operator data, that is related to the DA itself
         bytes calldata _totalL2ToL1PubdataAndStateDiffs
     ) external pure returns (bytes32 outputHash) {
-        // The Merkle path is required to verify the proof inclusion. The `outputHash` is used as a leaf in the Merkle tree.
-        // outputHash = keccak256(_totalL2ToL1PubdataAndStateDiffs);
+        
+        // I hard-coded the namespace, because it doesn't really matter for L2s like ZKStack
+        // However, it should be customizeable
+        // So I need to make it a config option
         Namespace memory ns = Namespace(0x00, 0x00000000000000000000000000000000000000000000000102030405);
         BlobInclusionProof memory payload = abi.decode(_totalL2ToL1PubdataAndStateDiffs, (BlobInclusionProof));
 
+        // We have a NamespaceMerkleMultiproof for each row spanned by the blob
+        // Here, we verify each of them
         uint start = 0;
         for (uint i = 0; i < payload.share_to_row_root_proofs.length; i++) {
             NamespaceMerkleMultiproof memory proof = payload.share_to_row_root_proofs[i];
             uint end = start + proof.endKey - proof.beginKey;
 
-            /* this can be optimized by using calldata,
-               but currently I am leveraging ABI deserialization for convenience
-               We may need to optimize later.
-            */
+            // Solidity doesn't let you slice arrays in memory
+            // We might want to optimize this, and avoid so much copying
             (bytes[] memory slice, DAVerifier.ErrorCodes err) = DAVerifier.slice(payload.blob, start, end);
+            require(err == DAVerifier.ErrorCodes.NoError, "DAVerifier: slice error");
             require(NamespaceMerkleTree.verifyMulti(
                 payload.rowRoots[i],
                 proof,
                 ns,
                 slice
-            ));
+            ), "Share to row root NMT multiproof failed");
             start = end;
         }
-
-        require(BinaryMerkleTree.verifyMulti(
-            payload.rowRoots,
+        
+        // For each row spanned by the blob, we have a row root
+        // The data root is the merkle root of the row + column roots
+        // Finally, we verify the multiproof for the row roots into the data root
+        require(BinaryMerkleTree.verifyMultiNamespaced(
+            payload.dataRoot,
             payload.row_inclusion_range_proof,
-            payload.dataRoot
-        ));
+            payload.rowRoots
+        ), "Row root to data root multiproof verification failed");
 
         outputHash = payload.dataRoot;
 
